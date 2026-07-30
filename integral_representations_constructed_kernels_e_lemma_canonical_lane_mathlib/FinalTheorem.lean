b5a556cf89a80d3403a510d25b301b1ad8e5be4e
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-29 21:11:26 -0400

    Canon

---
+++ integral_representations_constructed_kernels_e_lemma_canonical_lane_mathlib/FinalTheorem.lean
@@ -0,0 +1,9 @@
+import integral_representations_constructed_kernels_e_lemma_canonical_lane_mathlib.GateLemmas
+namespace HautevilleHouse
+namespace integral_representations_constructed_kernels_e_lemma_canonical_lane_mathlib
+structure ConstrainedTheoremClosure where
+  admissible :: intro :: constraint :: consequence :: proof ::
+theorem canonical : ConstrainedTheoremClosure :=
+  ConstrainedTheoremClosure.intro
+end integral_representations_constructed_kernels_e_lemma_canonical_lane_mathlib
+end HautevilleHouse
